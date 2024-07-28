fc2af6e72b748c75a15078020f13b738be8f6668
Merve Tansoy <157800967+mervetansoy@users.noreply.github.com>
2024-07-28 19:41:41 +0300

    agent controller

--- Assets/AgentController.cs
+++ Assets/AgentController.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class AgentController : MonoBehaviour
+public class AgentController : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+   
+   [SerializeField] private Transform hedef;
+
+    public override void OnEpisodeBegin()
     {
-        
+        transform.localPosition = new Vector3(-17f,0.24999997f,-32f);
+
+        int randm=Random.Range(0,2);
+        if(randm==0)
+        {
+            hedef.localPosition=new Vector3(-14f,0.5f,-32f);
+        }
+        if( randm ==1)
+        {
+            hedef.localPosition=new Vector3(-19.5f,0.5f,-32f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+       sensor.AddObservation(transform.localPosition);
+       sensor.AddObservation(hedef.localPosition);
     }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float move = actions.ContinuousActions[0];
+        float moveSpeed=2f;
+
+        transform.localPosition +=new Vector3(move,0f) * Time.deltaTime*moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousAction=actionsOut.ContinuousActions;
+        continuousAction[0]=Input.GetAxisRaw("Horizontal");
+    }
+
+    private void OnTriggerEnter(Collider nesne)
+ { 
+    if(nesne.gameObject.tag =="Potion")
+    {
+        AddReward(10f);
+        EndEpisode();
+    }
+    if (nesne.gameObject.tag == "Wall")
+    {
+        AddReward(-5f);
+        EndEpisode();
+    }
+ }    
 }
