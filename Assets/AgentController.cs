217524566c72c5a844bc9c643b263689ddd47882
Merve Tansoy <157800967+mervetansoy@users.noreply.github.com>
2024-07-29 15:41:21 +0300

    raycasthit

--- Assets/AgentController.cs
+++ Assets/AgentController.cs
@@ -11,29 +11,41 @@ public class AgentController : Agent
    [SerializeField] private Transform hedef;
    [SerializeField] private float moveSpeed =4f;
 
+
+   private Rigidbody rb;
+
+    public override void Initialize()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
     public override void OnEpisodeBegin()
     {
         transform.localPosition = new Vector3(Random.Range(-20f,-12f), 0.25f, Random.Range(-27f,-35f));
-        hedef.localPosition=new Vector3(Random.Range(-20f,-12f), 0.25f, Random.Range(-27f,-35f));
+        //hedef.localPosition=new Vector3(Random.Range(-20f,-12f), 0.25f, Random.Range(-27f,-35f));
         
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
        sensor.AddObservation(transform.localPosition);
-       sensor.AddObservation(hedef.localPosition);
+       //sensor.AddObservation(hedef.localPosition);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
-       
+        float moveRotate = actions.ContinuousActions[0];
+        float moveForward = actions.ContinuousActions[1];
 
+        rb.MovePosition(transform.position + transform.forward * moveForward * moveSpeed * Time.deltaTime);
+        transform.Rotate(0f, moveRotate * moveSpeed, 0f, Space.Self);
+
+        /*
         Vector3 velocity=new Vector3(moveX,0f,moveZ) ;
         velocity = velocity.normalized * Time.deltaTime * moveSpeed;
 
         transform.localPosition +=velocity;
+        */
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -47,7 +59,7 @@ public class AgentController : Agent
  { 
     if(nesne.gameObject.tag =="Potion")
     {
-        AddReward(2f);
+        AddReward(5f);
         EndEpisode();
     }
     if (nesne.gameObject.tag == "Wall")
